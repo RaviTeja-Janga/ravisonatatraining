f90c6e08260197e2de140146eb525f87891c5074
hydtrng <hydtrng@SONATA.LOCAL>
2022-08-24 11:33:39 +0530

    newone

---
+++ day_2/day2.py
@@ -0,0 +1,7 @@
+import main
+import maximum
+import palindrome
+import scissor
+import split
+import words
+
---
+++ day_2/split.py
@@ -0,0 +1,6 @@
+def count(a):
+    b=a.split()
+    return len(b)
+
+str=count('raviteja is a good boy')
+print(str)
